63c9eb8a052bde40a856a4ea2688e3085ba765ec
mikolaj <mik.gurgul@gmail.com>
2023-05-20 00:17:00 +0200

    fight service test

--- src/test/java/org/gm/fights/FightServiceTest.java
+++ src/test/java/org/gm/fights/FightServiceTest.java
@@ -1,4 +1,61 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.gm.fights;
+
+import org.gm.hero.entity.Hero;
+import org.gm.hero.entity.HeroClass;
+import org.gm.monster.entity.Monster;
+import org.gm.monster.entity.MonsterClass;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class FightServiceTest {
-  
+    @Mock
+    private Hero hero;
+    @Mock
+    private Monster monster;
+    @InjectMocks
+    FightService fightService;
+
+    @BeforeEach
+    public void setUp() {
+        hero = new Hero("Archie", HeroClass.ARCHER);
+
+        monster = new Monster("Higher Orc Commander", MonsterClass.ORC);
+        monster.setLvl(1);
+        monster.setHp(50);
+        monster.setExperience(100);
+        monster.setDamage(30);
+
+        fightService = new FightService();
+    }
+
+    @Test
+     void testFight_HeroWins() {
+
+        //given
+        hero.setDamage(40);
+
+        //when
+        fightService.performBattle(hero, monster);
+
+
+        //then
+        assertTrue(hero.getHp() >= 0);
+    }
+
+    @Test
+     void testFight_MonsterWins() {
+
+        //given
+        hero.setDamage(10);
+
+        //when
+        fightService.performBattle(hero, monster);
+
+        //then
+        assertTrue(hero.getHp() <= 0);
+    }
 }
