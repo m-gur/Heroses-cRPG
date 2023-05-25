bcc7d392a55a78358d38e794fd7ff19526b304f9
mikolaj <mik.gurgul@gmail.com>
2023-05-25 22:21:42 +0200

    adding a welcome speech, menu option modification

--- src/main/java/org/gm/menu/Menu.java
+++ src/main/java/org/gm/menu/Menu.java
@@ -21,14 +21,17 @@ public class Menu {
         Scanner scanner = new Scanner(System.in);
         Map<Integer, Runnable> menuOptions = new HashMap<>();
         menuOptions.put(1, this::createHero);
+        menuOptions.put(2, this::loadGame);
 
         System.out.println("Welcome to the Heroes!");
         int choice;
 
         do {
-            System.out.println("Please enter the number to use the option:");
-            System.out.println("1. Create hero");
-            System.out.println("2. Exit the game");
+            System.out.println("""
+                    Please enter the number to use the option:
+                    1. Create new hero
+                    2. Load game
+                    3. Exit the game""");
 
             choice = scanner.nextInt();
             scanner.nextLine();
@@ -38,31 +41,37 @@ public class Menu {
             } else {
                 System.out.println("Invalid choice. Please try again.");
             }
-        } while (choice != 2);
+        } while (choice != 3);
+    }
+    //TODO create logic to loading game
+    private void loadGame() {
+
     }
 
     private void createHero() {
         Scanner scanner = new Scanner(System.in);
         int choice;
         do {
-            System.out.println("Which class of hero you want to choose? \n" +
-                    "1. Archer \n" +
-                    "2. Knight \n" +
-                    "3. Mage \n" +
-                    "4. Exit");
+            System.out.println("""
+                    Which class of hero you want to choose?
+                    1. Archer
+                    2. Knight
+                    3. Mage
+                    4. Return
+                    """);
 
             choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
-                case 1 -> createHeroNext(new Archer());
-                case 2 -> createHeroNext(new Knight());
-                case 3 -> createHeroNext(new Mage());
+                case 1 -> exploreCity(createHeroPartTwo(new Archer()));
+                case 2 -> exploreCity(createHeroPartTwo(new Knight()));
+                case 3 -> exploreCity(createHeroPartTwo(new Mage()));
                 default -> System.out.println("Invalid choice. Please try again.");
             }
         } while (choice != 4);
     }
 
-    private Hero createHeroNext(Hero hero) {
+    private Hero createHeroPartTwo(Hero hero) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your Hero name: ");
         hero.setName(scanner.nextLine());
@@ -72,6 +81,16 @@ public class Menu {
         hero.setCurrentHp(hero.getMaxHp());
         abilitiesService.setModifierAbilities(hero);
         abilitiesService.setAbilitiesAfterModifier(hero);
+
+        System.out.println("Welcome " + hero.getName() + "! \n" +
+                "I am really glad to see you want to spent a few great moments in our world.\n" +
+                "However you must to know this city has a problem with monsters..\n" +
+                "There are some types of them, some of them are familiar to city dwellers.\n" +
+                "They can help you to describe and got needed staff to explore the world.\n" +
+                "Unfortunately there are a few monsters, which are really dangerous..\n" +
+                "Their goal is to destroy this world, getting armies and start the war with humans!\n" +
+                "You must help this people survive, find the solution...");
+
         return hero;
     }
 }
