8abe5d982a289f74dcc3a16ea1c53407590ff4da
mikolaj <mik.gurgul@gmail.com>
2023-05-29 20:19:18 +0200

    accumulateExperience method test added

---
+++ src/test/java/org/gm/hero/services/LevelServiceTest.java
@@ -0,0 +1,34 @@
+package org.gm.hero.services;
+
+import org.gm.hero.entity.Archer;
+import org.gm.hero.entity.Hero;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class LevelServiceTest {
+
+    @Mock
+    Hero hero;
+    @InjectMocks
+    LevelService levelService;
+    @BeforeEach
+    public void setUp() {
+        levelService = new LevelService();
+        hero = new Archer("Archie");
+        hero.setRequiredExperience(levelService.calculateRequiredExperience(hero.getLvl()));
+    }
+    @Test
+    void accumulateExperience_withoutParameters_returnsTrue() {
+        //given
+        float experience = 1000;
+        //when
+        levelService.accumulateExperience(hero, experience);
+        //then
+        assertTrue(hero.getLvl() >= 4);
+        assertTrue(hero.getExperience() <= 300);
+    }
+}
