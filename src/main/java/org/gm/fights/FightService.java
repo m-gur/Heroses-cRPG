34480c7097f1310312823a28acd59591006046eb
mikolaj <mik.gurgul@gmail.com>
2023-05-30 22:02:52 +0200

    adding a random item after winning a fight

--- src/main/java/org/gm/fights/FightService.java
+++ src/main/java/org/gm/fights/FightService.java
@@ -1,15 +1,21 @@
 package org.gm.fights;
 
+import org.gm.factory.ItemFactory;
 import org.gm.hero.entity.Hero;
+import org.gm.hero.items.entity.Item;
+import org.gm.hero.items.entity.ItemType;
 import org.gm.hero.services.LevelService;
 import org.gm.monster.entity.Monster;
 
-import java.util.Random;
+import java.util.*;
 
 public class FightService {
     private LevelService levelService = new LevelService();
+    private ItemFactory itemFactory = new ItemFactory();
     public void performBattle(Hero hero, Monster monster) {
         Random random = new Random();
+        List<Item> itemList = new ArrayList<>();
+        Map<ItemType, List<Item>> listMap = new HashMap<>();
         boolean heroFirstAttack = random.nextBoolean();
 
         while (hero.getCurrentHp() > 0 && monster.getHp() > 0) {
@@ -17,6 +23,10 @@ public class FightService {
                 performHeroAttack(hero, monster);
                 if (monster.getHp() <= 0) {
                     levelService.accumulateExperience(hero, monster.getExperience());
+                    Item randomItem = itemFactory.createRandomItem(hero);
+                    itemList.add(randomItem);
+                    listMap.put(randomItem.getItemType(), itemList);
+                    hero.setInventory(listMap);
                 } else {
                     performMonsterAttack(hero, monster);
                 }
@@ -28,6 +38,10 @@ public class FightService {
                     performHeroAttack(hero, monster);
                     if (monster.getHp() <= 0) {
                         levelService.accumulateExperience(hero, monster.getExperience());
+                        Item randomItem = itemFactory.createRandomItem(hero);
+                        itemList.add(randomItem);
+                        listMap.put(randomItem.getItemType(), itemList);
+                        hero.setInventory(listMap);
                     }
                 }
             }
